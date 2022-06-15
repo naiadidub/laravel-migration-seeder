92d1b11eb559627f382c06d1d31c1d45f0bb1681
naiadidub <antonio.zaghi1405@gmail.com>
2022-06-15 09:22:38 +0200

    the end

--- migration/resources/views/home.blade.php
+++ migration/resources/views/home.blade.php
@@ -7,17 +7,20 @@
 <div class="container">
     <div class="row">
         @foreach ($viaggi as $item){
-            <div class="col">
-                <div class="card" style="width: 18rem;">
-                    <div class="card-body">
-                      <h5 class="card-title">{{$item->title}}</h5>
-                      <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
-                      <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                      <a href="#" class="card-link">Card link</a>
-                      <a href="#" class="card-link">Another link</a>
+            <a href="{{route('detail', $item->id)}}" style="text-decoration: none">
+                <div class="col">
+                    <div class="card" style="width: 18rem;">
+                        <div class="card-body">
+                        <h5 class="card-title">{{$item->title}}</h5>
+                        <h6 class="card-subtitle mb-2 text-muted">prezzo ${{$item->prezzo}}</h6>
+                        <p>partenza:{{$item->luogo_di_partenza}}</p>
+                        <p>arrivo:{{$item->luogo_di_atterraggio}}</p>
+                        <p>il:{{$item->data_partenza}}</p>
+                        <p>alle:{{$item->ora_partenza}}</p>
+                        </div>
                     </div>
-                  </div>
-            </div>
+                </div>
+            <a>
         }
             
         @endforeach
